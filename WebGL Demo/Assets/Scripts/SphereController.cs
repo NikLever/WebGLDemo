1c6c915d26dc1b3935f896b7619ea43891fd94ca
Nicholas Lever <nik.j.lever@gmail.com>
2023-03-08 10:17:51 +0000

    Initial commit

---
+++ WebGL Demo/Assets/Scripts/SphereController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class SphereController : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void LogString(string str);
+
+    private void Start()
+    {
+        //SetHeight(3);
+        Application.runInBackground = true;
+    }
+
+    public void SetHeight( float height )
+    {
+        string str = System.String.Format("SetHeight called with height {0}", height);
+        LogString(str);
+        Debug.Log(System.String.Format("Debug.Log: {0}", str));
+        Vector3 pos = transform.position;
+        pos.y = height;
+        transform.position = pos;
+    }
+}
